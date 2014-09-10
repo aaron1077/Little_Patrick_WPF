6897e7eabb7d6e43d7f9a84ed667e458fb07d535
aaron1077 <aaron1077>
2014-09-10 16:28:04 -0500

    Calculated users age
    Calculated users age

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -13,4 +13,18 @@ var name = prompt("Please type in your name:");
 console.log(name);
 
 //Alert to user and explain what we are doing
-alert("Welcome "+name+"! Let's go ahead and figure out how old you are");
+alert("Welcome "+name+"! Let's go ahead and figure out how old you are!");
+
+//Ask the user what year they were born in
+//Create a variable to catch the answer
+var yearBorn = prompt("What year were you born?");
+console.log(yearBorn);
+
+//Current year - year born
+var currentYear = 2014;
+
+//Calculate the age
+var age = currentYear - yearBorn-1;
+
+//alert the user with their age
+alert(name+" you are "+age+" years old.");
