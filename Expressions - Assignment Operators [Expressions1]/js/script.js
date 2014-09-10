8cbf5144ed19e0ea36f365991694ad89f3fa8467
aaron1077 <aaron1077>
2014-09-09 23:05:44 -0500

    Expressions - Assignment Operators
    Expressions - Assignment Operators

---
+++ Expressions - Assignment Operators [Expressions1]/js/script.js
@@ -0,0 +1,8 @@
+//Expressions - Assignment Operators
+
+var a = 3; // variable a
+var b = 4; //variable b
+b ++; // b = b + 1 or b = +=1
+a += 4; //a = a + 4
+console.log(a); //write a
+console.log(b); //write b
