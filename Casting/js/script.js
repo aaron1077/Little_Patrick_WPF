dab3617a6a8d16668aa0daf5fe694ca075a0aae1
aaron1077 <aaron1077>
2014-09-09 22:50:54 -0500

    Casting
    Learning Casting

---
+++ Casting/js/script.js
@@ -0,0 +1,12 @@
+//Casting
+
+var stringVar = "6"; //variable string
+var result = 7 + Number(stringVar); //variable result
+console.log(result); //write result
+
+var areaCode = 407; //variable areaCode
+var firstPart = 203; //variable firstPart
+var secPart = 4529; //variable secPart
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart); //write phoneNo
+console.log(phoneNo); //write phoneNo
