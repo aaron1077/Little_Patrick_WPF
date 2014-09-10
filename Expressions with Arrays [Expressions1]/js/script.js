92a3c959d6aa1071456ffb43a7e7fb782b89b677
aaron1077 <aaron1077>
2014-09-09 22:08:20 -0500

    Expressions with Arrays
    Created a document that showed me use Expressions with Arrays

---
+++ Expressions with Arrays [Expressions1]/js/script.js
@@ -0,0 +1,6 @@
+//Expressions - Expressions using Arrays
+
+var orangeBins = [234, 567, 883]; // variable orangeBins
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2]; //variable total
+console.log(total); //write total
