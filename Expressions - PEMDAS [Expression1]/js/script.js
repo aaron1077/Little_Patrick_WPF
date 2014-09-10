986af15d07edccecaaecd8ca7ea322da7c3f4e73
aaron1077 <aaron1077>
2014-09-09 21:52:00 -0500

    Create a js file using PEMDAS Expressions
    Create script.js using PEMDAS Expressions

---
+++ Expressions - PEMDAS [Expression1]/js/script.js
@@ -0,0 +1,12 @@
+// Expressions - PEMDAS - The Order of Operations
+
+var quiz1 = 87; //Variable quiz1 = 87
+var quiz2 = 100; //Variable quiz2 = 100
+var quiz3 = 60; // Variable quiz3 = 60
+var quiz4= 80; //Variable quiz4 = 80;
+// the average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4; //Variable average is (quiz1 + quiz2 + quiz3 + quiz4)/4
+console.log(average); //write the average
+var perimeter = length * 2 + width * 2; // variable perimeter equals length times 2 plus width times 2
+console.log(perimeter); //write perimeter
