36f774290ccfdb7bbc9f6d43ee6b190c116d1845
aaron1077 <aaron1077>
2014-09-09 20:20:01 -0500

    Added arrays to script.js
    Added arrays and to script.js in my Variables - Defining folder

--- Variables - Defining/js/script.js
+++ Variables - Defining/js/script.js
@@ -2,3 +2,10 @@ var age = 21;
 var isStudent = false; //Boolean Variable
 var firstName = "Patrick";
 var phrase = 'I don\'t know \n you\'ll have to speak with the professor';
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+muppetNames[1] = "Elmo"; //changes index #1 to Elmo
+muppetNames[2] = "Cookie monster"; // changes index #2 to Cookie monster
+var num = 1;
+console.log(muppetNames[num]); //lets me see whats in index[1]
+
