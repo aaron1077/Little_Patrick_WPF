32db7a196cff2af2b602231422c12381ce5e0c5e
aaron1077 <aaron1077>
2014-09-10 16:20:42 -0500

    Prompt user for name
    Prompt user for name

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -3,3 +3,14 @@
 //Sept 10 2014
 //WPF Section 02
 //alert("hello");
+
+// Create a calculator for age
+
+//Ask the user their name
+var name = prompt("Please type in your name:");
+
+//Test to make sure this works
+console.log(name);
+
+//Alert to user and explain what we are doing
+alert("Welcome "+name+"! Let's go ahead and figure out how old you are");
