aaecae8add8a4bc15174d9d6637823299662c790
aaron1077 <aaron1077>
2014-09-09 21:03:28 -0500

    Expressions - Age Example
    Expressions - Age Example

---
+++ Expressions - Age Example/js/script.js
@@ -0,0 +1,3 @@
+var yearBorn = 1992;
+var age = 2014 - yearBorn - 1;
+console.log(age);
--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -1,4 +1,4 @@
 var a = 2; //sets variable defines a as 2
 var b; //variable b
 b = a + 3; //add 3 to a
-console.log(a); //write a 
+console.log(a); //write a
