39d0bc58967a08c09d512703ab9f5f06a0517614
aaron1077 <aaron1077>
2014-09-09 22:34:35 -0500

    Creating Expressions - Concatenation strings
    Creating Expressions - Concatenation strings

---
+++ Expressions - Concatenation [Expressions1]/js/script.js
@@ -0,0 +1,13 @@
+//Expressions - Expressions using Concatenation strings
+
+var firstName = "Patrick"; //variable firstName
+var lastName = "Little"; // Variable lastName
+var fullName = firstName + " " + lastName; //variable fullName
+
+console.log(fullName); //write fullName
+
+var a = "6"; //variable a
+var b = "7"; //variable b
+
+var result = a + b; //variable result
+console.log(result); //write result
