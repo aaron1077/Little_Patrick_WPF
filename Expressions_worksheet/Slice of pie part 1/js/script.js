6d650946f7e13c7114ea632834837b60202fa829
aaron1077 <aaron1077>
2014-09-10 20:05:48 -0500

    Created 3 variables
    Created variables for number of slices, number of people, and number of
    pizzas

--- Expressions_worksheet/Slice of pie part 1/js/script.js
+++ Expressions_worksheet/Slice of pie part 1/js/script.js
@@ -3,3 +3,11 @@
 //Expressions_worksheet
 //Slice of pie part 1
 
+//number of slices per pizza
+var numberOfSlices = 8;
+
+//number of people at the party
+var numberOfPeople = 16;
+
+//number of pizzas ordered
+var numberOfPizzas = 2;
