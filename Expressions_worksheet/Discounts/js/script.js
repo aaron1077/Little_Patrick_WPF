34672634cee67918a342b2d80624b08dcfe4498c
aaron1077 <aaron1077>
2014-09-10 21:50:10 -0500

    Added the result and price with and without tax
    Added the variables with and without tax and printed the results

--- Expressions_worksheet/Discounts/js/script.js
+++ Expressions_worksheet/Discounts/js/script.js
@@ -10,7 +10,29 @@ var original = 200;
 var discount = 20;
 
 //Description of item
-console.log(" Your item is a 1/3 carrot diamond ring. this item has a 20% discount");
+var description = "Diamond ring";
 
 //sales tax percentage
 var saleTax = .08;
+
+//the discount
+var discountResult = original/discount;
+
+//price of the item without tax
+var priceWithoutTax = original - discountResult;
+
+//the tax
+var saleTaxResult = priceWithoutTax * saleTax;
+
+//price of the item with tax
+var priceWithTax = Number(saleTaxResult) + Number(priceWithoutTax);
+
+
+
+
+
+//print the result
+console.log("Your " +description+ " was originally $"+original+", but after a " +discount+"% discount, it is now $"+priceWithoutTax+ " without tax, and $"+priceWithTax+ " with tax.");
+
+
+
