22c37884f8e3a4e6fa11d05d349095f73b6b4d6b
aaron1077 <aaron1077>
2014-09-10 21:21:02 -0500

    Created my givens
    Created my givens

--- Expressions_worksheet/Discounts/js/script.js
+++ Expressions_worksheet/Discounts/js/script.js
@@ -3,3 +3,14 @@
 //Expressions_Worksheet
 //Discounts
 
+//Original price
+var original = 200;
+
+//discount percentage
+var discount = 20;
+
+//Description of item
+console.log(" Your item is a 1/3 carrot diamond ring. this item has a 20% discount");
+
+//sales tax percentage
+var saleTax = .08;
