5f94df66b3fee86865ece9d5be2fc7cdbf97c717
aaron1077 <aaron1077>
2014-09-11 19:32:52 -0500

    Added variable and console.log
    Added a variable perHour and a consol.log

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,6 +6,11 @@
 // stating an alert, how much money you earn in a given time
 alert("We are going to figure out how much money you earn in a period of time before taxes.");
 
+// Variable saying how much money you make an hour
+var perHour = prompt("How much money do you make an hour?");
+
+//print the amount of hours
+console.log(perHour);
 
 
 
