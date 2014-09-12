806eae8b8ce7891df421e79d23b44ea093f062d7
aaron1077 <aaron1077>
2014-09-11 19:31:42 -0500

    making my first alert
    Making my first alert for personal folder

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,6 +3,10 @@
 //Activity Expression worksheet
 //Expressions_Personal
 
+// stating an alert, how much money you earn in a given time
+alert("We are going to figure out how much money you earn in a period of time before taxes.");
+
+
 
 
 
