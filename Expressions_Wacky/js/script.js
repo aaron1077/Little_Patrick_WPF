a1444986540bfbf03581ed8482ff442a3d564264
aaron1077 <aaron1077>
2014-09-11 21:36:06 -0500

    Added first alert
    Added first alert in expressions_wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,10 @@
 //Patrick Little
-//Sept. 11 2014
+//Sept. 11, 2014
 //Assignment: Expressions
 //Expressions_Wacky
+
+// first alert
+alert("We are going to find the perimeter of your house and shed and add them together in feet.");
+
+
+
