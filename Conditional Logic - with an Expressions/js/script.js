8b9b21ccdc8a6cbcf155e29dddc1aaa90993c526
aaron1077 <aaron1077>
2014-09-15 10:43:17 -0500

    Conditional Logic With an Expression
    Conditional Logic With an Expression

---
+++ Conditional Logic - with an Expressions/js/script.js
@@ -0,0 +1,12 @@
+//Conditional Logic - with an expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride"
+//if the kid is over 48inches in height
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("you can ride the coaster");
+}
