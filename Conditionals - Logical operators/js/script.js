459ea5dd9e7620460d01990c07ca8a8277d081fd
aaron1077 <aaron1077>
2014-09-15 11:43:36 -0500

    Conditional logical operators
    the or symbol

--- Conditionals - Logical operators/js/script.js
+++ Conditionals - Logical operators/js/script.js
@@ -1,11 +1,11 @@
 //Conditional logic - logical operators
 
-var budget = 300;
+var budget = 100;
 var iphonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less our budget and if our paycheck is over 300
-if(iphonePrice < budget && paycheck > 300){
+if(iphonePrice < budget || wonLottery){
     console.log("we can buy the phone");
 }else{
     console.log("no phone for you");
