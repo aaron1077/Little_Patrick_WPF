53aabf79dc5e6c594daf7a37ee36b8dd59861f42
aaron1077 <aaron1077>
2014-09-15 10:38:29 -0500

    Conditionals with relational expressions
    Made a js and html file for conditionals with relational expressions

---
+++ Conditionals - with relational expressions/js/script.js
@@ -0,0 +1,11 @@
+//Conditional Logic - Relational Expressions
+
+var kidHeight = 50;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride"
+//if the kid is over 48inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("you can ride the coaster");
+}
