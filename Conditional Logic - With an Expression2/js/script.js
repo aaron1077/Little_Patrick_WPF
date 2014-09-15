89689f9467a983b6dda73109b581ee629b345c6b
aaron1077 <aaron1077>
2014-09-15 10:50:59 -0500

    Conditional Logic - With an Expression2
    Conditional Logic - With an Expression 2, if and else

---
+++ Conditional Logic - With an Expression2/js/script.js
@@ -0,0 +1,16 @@
+//Conditional Logic - with an expression2
+
+var kidHeight = 52;
+var minHeight = 48;
+
+
+//if the child is old enough, print to the console "you can ride"
+//if the kid is over 48inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("you can ride the coaster");
+}else{
+    //code performed if condition is false
+    console.log("sorry kid, youve got some growing to do first.");
+
+}
