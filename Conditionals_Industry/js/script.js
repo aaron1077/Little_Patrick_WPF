1e832494a8d405b375688c459e6c904546457857
aaron1077 <aaron1077>
2014-09-17 18:36:21 -0500

    Created first prompt
    Created first prompt  for Conditionals_Industry

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,7 @@
+//Patrick Little
+//Sept. 17 2014
+//Assignment conditionals
+//Conditionals_Industry
+
+//first prompt
+var firstPrompt = prompt("Were going to find out if you qualify for our Web Design and Development job.");
