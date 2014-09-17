1a03dc434b989cf4557c51eec4db053c3da4803a
aaron1077 <aaron1077>
2014-09-17 18:44:23 -0500

    Created first prompt and years of work variable
    Created first prompt and years of work variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,5 +3,11 @@
 //Assignment conditionals
 //Conditionals_Industry
 
+//variable
+var yearsOfWork = 1;
+//first alert
+var firstAlert = alert("Were going to find out if you qualify for our Web Design and Development job, lets begin the interview.");
 //first prompt
-var firstAlert = alert("Were going to find out if you qualify for our Web Design and Development job.");
+var firstPrompt = prompt("How many years have you been working on web development?")
+
+
