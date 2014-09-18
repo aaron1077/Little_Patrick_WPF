ab55e55edde730f1e16e62804df9703787e6749a
aaron1077 <aaron1077>
2014-09-17 19:38:20 -0500

    added an else if statement
    added an else if statement and printed

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,16 +6,21 @@
 //variable firstCheck
 var firstCheck = "";
 //first alert
-var firstAlert = alert("Were going to find out if you qualify for our Web Design and Development job, lets begin the interview.");
+alert("Were going to find out if you qualify for our Web Design and Development job, lets begin the interview.");
 //first prompt
-var firstPrompt = prompt("How many years have you been working on web development?")
+var firstPrompt = prompt("How many years have you been working on web development?");
 //Print firstPrompt
 console.log(firstPrompt);
 //if statement saying if firstPrompt is less than 2 thank print to the console
-if(firstPrompt < 2){
-    alert("Hmm, I would like to see more experience but we will see how the rest of the interview goes.")
-}else if(firstPrompt = firstCheck){
-    prompt("You forgot to input something.")
+if(firstPrompt < 2 && firstPrompt > 0){
+    //Create an alert for the concern of experience
+    alert("Hmm disappointing.");
+    //else if statement in case they entered nothing
+}else if(firstPrompt == firstCheck){
+    //Prompt for if they don't input anything
+    var pleaseInput = prompt("Please input something?");
+    //Print the number entered
+    console.log(pleaseInput);
 }
 
 
