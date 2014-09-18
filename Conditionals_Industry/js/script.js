5c2b259a9173bc0f7be29340a250e7041a7240be
aaron1077 <aaron1077>
2014-09-17 19:01:29 -0500

    Added an if statement for my first prompt
    Added an if statement for my first prompt

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,11 +3,19 @@
 //Assignment conditionals
 //Conditionals_Industry
 
-//variable
-var yearsOfWork = 1;
+//variable firstCheck
+var firstCheck = "";
 //first alert
 var firstAlert = alert("Were going to find out if you qualify for our Web Design and Development job, lets begin the interview.");
 //first prompt
 var firstPrompt = prompt("How many years have you been working on web development?")
+//Print firstPrompt
+console.log(firstPrompt);
+//if statement saying if firstPrompt is less than 2 thank print to the console
+if(firstPrompt < 2){
+    alert("Hmm, I would like to see more experience but we will see how the rest of the interview goes.")
+}else if(firstPrompt = firstCheck){
+    prompt("You forgot to input something.")
+}
 
 
