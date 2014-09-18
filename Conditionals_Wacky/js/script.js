aa6234a7395ac7bab51a0a6997758c8bcc012df3
aaron1077 <aaron1077>
2014-09-18 18:59:22 -0500

    Added variable tall
    Added variable tall

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,5 +9,7 @@ var error = "";
 alert("If a donkey is older than 2 years old or 2 feet tall it can rome free in the pasture.");
 //prompt stating how old the donkey is
 var age = prompt("How old is your donkey?");
+//prompt stating how tall your donkey is
+var tall = prompt("How tall is your donkey?");
 
 
