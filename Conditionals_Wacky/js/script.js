5d46f25fa80326d3149c34b57df59482962182ad
aaron1077 <aaron1077>
2014-09-18 19:04:53 -0500

    Added variable result and if statement
    Added variable result and if statement

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -11,5 +11,12 @@ alert("If a donkey is older than 2 years old or 2 feet tall it can rome free in 
 var age = prompt("How old is your donkey?");
 //prompt stating how tall your donkey is
 var tall = prompt("How tall is your donkey?");
+//variable result
+var result;
+//if  age == error or tall == error the prompt the message
+if(age == error || tall == error ){
+    //prompt the message
+    var errorMessage = prompt("enter your answer please.");
+}
 
 
