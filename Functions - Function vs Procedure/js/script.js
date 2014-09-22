8adcf54dac0e3546b8c7f6afa696c78a062f41ac
aaron1077 <aaron1077>
2014-09-22 14:10:11 -0500

    Functions - Functions vs Procedures
    Functions - Functions vs Procedures

---
+++ Functions - Function vs Procedure/js/script.js
@@ -0,0 +1,13 @@
+//Functions - Function vs Procedure
+
+//this is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area;
+}
+
+//This is a procedure
+function calcArea(width, height){
+    var area = width * height;
+    console.log(area);//no return
+}
