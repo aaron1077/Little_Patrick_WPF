233cebe913b1bd0debc7c4f85e2fba779e3f01f5
aaron1077 <aaron1077>
2014-09-22 14:16:48 -0500

    Functions - Anonymous Functions
    Functions - Anonymous Functions

---
+++ Functions - Anonymous Functions/js/script.js
@@ -0,0 +1,11 @@
+//Functions - Anonymous Functions
+
+
+var calcArea = function(width, height){//defining
+    //code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30);//invoking
+console.log(a);
