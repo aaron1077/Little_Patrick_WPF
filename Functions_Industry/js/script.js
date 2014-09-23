7ae8984b3b978f576f252f7a0af203194a0d299d
aaron1077 <aaron1077>
2014-09-23 18:54:54 -0500

    Create function polygonArea
    Create function polygonArea with variable area and return area inside
    with comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,3 +8,9 @@ alert("We are going to find the area of a regular polygon");
 //variable total
 var total = polygonArea(prompt("What is the apothem in inches?"), prompt("What is the perimeter in inches?"));
 //function polygonArea
+function polygonArea(a, p){
+    //variable area
+    var area = 1/2 * a * p;
+    //return area
+    return area;
+}
