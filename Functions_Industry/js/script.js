d24b2e2626061b14e17c60d74eb577e8d2a76a8c
aaron1077 <aaron1077>
2014-09-23 18:54:02 -0500

    Create comment for function polygonArea
    Create comment for function polygonArea

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,3 +7,4 @@
 alert("We are going to find the area of a regular polygon");
 //variable total
 var total = polygonArea(prompt("What is the apothem in inches?"), prompt("What is the perimeter in inches?"));
+//function polygonArea
