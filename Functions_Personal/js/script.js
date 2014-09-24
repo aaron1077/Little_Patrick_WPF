f525191c3c078682416e7833786921f199f7e9e2
aaron1077 <aaron1077>
2014-09-23 20:05:31 -0500

    Created function within fruits variable
    Created function within fruits variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,6 +4,10 @@
 //Personal
 
 alert("You bought a bag of apples, oranges, and bananas. We are going to find out how many pieces of fruits you have.");
+var fruits = function (a, o, b){
+    var howMany = Number(a) + Number(o) + Number(b);
+    return howMany;
+}
 
 
 
