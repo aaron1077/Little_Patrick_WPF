36fd5dc8bf16e7f4144f41a90fddcfa6f9986622
aaron1077 <aaron1077>
2014-09-23 21:02:56 -0500

    Added first two comments
    Added first 2 comments

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,8 +3,9 @@
 //Assignment:Function
 //Function Wacky
 
+//alert to see if you are eligible for my club
 alert("Were going to see if you are eligible to join my club.");
-
+//clubJoin which is a function and stating 2 prompts
 clubJoin(prompt("What is you favorite number?"), prompt("How old are you?"));
 
 function clubJoin(f, o){
