f886dc106b858543eebc7f7538c0afb63cc9b88b
aaron1077 <aaron1077>
2014-09-23 21:01:30 -0500

    Added function clubJoin
    Added function clubJoin plus if else statement inside

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,4 +7,15 @@ alert("Were going to see if you are eligible to join my club.");
 
 clubJoin(prompt("What is you favorite number?"), prompt("How old are you?"));
 
+function clubJoin(f, o){
+    if(f < 30 && o >= 18 ){
+        var congrats = alert("Congrats you have made it in my club!");
+        return congrats;
+
+    }else{
+        var sorry = alert("sorry you are not qualified for my club.");
+        return sorry;
+
+    }
+}
 
