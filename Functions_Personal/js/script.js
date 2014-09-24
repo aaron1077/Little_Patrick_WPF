753850cf18d8e7c61fca5961de251348e19d5ba0
aaron1077 <aaron1077>
2014-09-23 20:09:41 -0500

    Created comment for alert and spacing for comments
    Created comments for alert and spacing for comments

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,12 +3,18 @@
 //Assignment:Functions
 //Personal
 
+//Created first alert
 alert("You bought a bag of apples, oranges, and bananas. We are going to find out how many pieces of fruits you have.");
+
 var fruits = function (a, o, b){
+
     var howMany = Number(a) + Number(o) + Number(b);
+
     return howMany;
 }
+
 var total = fruits(prompt("How many apples did you have?"), prompt("How many oranges did you have?"), prompt("How many bananas did you have?"));
+
 console.log("You have a total of " +total+ " fruits.");
 
 
